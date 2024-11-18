df3fe8991162562709451d47e03880b827b08f9a
Jarmo Annunen <jarmo@annunen.fi>
2024-11-18 11:01:30 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/yatzy-score-manager/sw.js', { scope: '/yatzy-score-manager/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jatsapp/sw.js', { scope: '/jatsapp/' })})}
