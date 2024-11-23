7be643aeca98ace92f597657ec371033f9a171fd
Jarmo Annunen <jarmo@annunen.fi>
2024-11-23 22:04:54 +0200

    Changed dist folder

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jatsapp/sw.js', { scope: '/jatsapp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
